37282b357d78977c7835176493a0f699e211f9be
Marko Man <darkobas@gmail.com>
2017-03-28 01:29:27 +0200

    remove priveleged module

--- Android.mk
+++ Android.mk
@@ -22,7 +22,6 @@ LOCAL_AAPT_FLAGS := \
         --version-code $(version_code_package) \
 
 LOCAL_PACKAGE_NAME := SnapdragonCameraCAF
-LOCAL_PRIVILEGED_MODULE := true
 
 #LOCAL_SDK_VERSION := current
 LOCAL_RENDERSCRIPT_TARGET_API := 23
