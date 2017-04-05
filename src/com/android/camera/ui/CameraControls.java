bc82ed6cc37392f588be87fb300845586ebeb754
Marko Man <darkobas@gmail.com>
2017-04-05 12:05:15 +0200

    Camera2: fix video shutter rotation

--- src/com/android/camera/ui/CameraControls.java
+++ src/com/android/camera/ui/CameraControls.java
@@ -1001,7 +1001,7 @@ public class CameraControls extends RotatableLayout {
         View[] views = {
             mSceneModeSwitcher, mFilterModeSwitcher, mFrontBackSwitcher,
             TsMakeupManager.HAS_TS_MAKEUP ? mTsMakeupSwitcher : mHdrSwitcher,
-            mMenu, mShutter, mPreview, mSwitcher, mMute, mReviewRetakeButton,
+            mMenu, mShutter, mVideoShutter, mPreview, mSwitcher, mMute, mReviewRetakeButton,
             mReviewCancelButton, mReviewDoneButton
         };
         for (View v : views) {
